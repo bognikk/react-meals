e98c5ddeb0f7b381debae96932ec2d9b8397e9f9
Nikola Bogicevic <nbogicevic2@gmail.com>
2021-10-04 15:59:33 -0400

    added a Cart Reducer

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,10 +1,14 @@
 import { useContext } from "react";
-import CardContext from "../../store/cart-context";
+import CartContext from "../../store/cart-context.js";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon.jsx";
 
 const HeaderCartButton = ({ onClick }) => {
-  const cartCtx = useContext(false);
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
   return (
     <button className={classes.button} onClick={onClick}>
@@ -12,7 +16,7 @@ const HeaderCartButton = ({ onClick }) => {
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
