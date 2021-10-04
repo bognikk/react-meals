41e1db6f269fdaa1fe480fcdd84229888fc77d3d
Nikola Bogicevic <nbogicevic2@gmail.com>
2021-10-04 10:18:31 -0400

    added a Cart context

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
+import CardContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon.jsx";
 
 const HeaderCartButton = ({ onClick }) => {
+  const cartCtx = useContext(false);
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
