bb7f1814269883cc9cf597a303a154dec520ba95
Nikola Bogicevic <nbogicevic2@gmail.com>
2021-10-05 17:32:23 -0400

    enhanced the Cart Reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,9 +8,27 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingcartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingcartItem) {
+      const updatedItem = {
+        ...existingcartItem,
+        amount: existingcartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
